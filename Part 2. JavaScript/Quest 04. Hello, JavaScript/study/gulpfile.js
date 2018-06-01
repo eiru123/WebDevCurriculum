dbbc8ad4025f661315902beb6ae35421b1156dab
SeungLee <ls0408@naver.com>
2018-06-01 15:33:36 +0900

    update files

---
+++ Part 2. JavaScript/Quest 04. Hello, JavaScript/study/gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+
+gulp.task('default', function(){
+    // node source
+    // babel은 es6 코드를 es5 코드로 변환
+    // gulp 는 디렉토리 구조와 파일 이름을 그대로 유지한다.
+    gulp.src("es6/**/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("dist"));
+
+    // browser source
+    // ** 은 서브디렉토리를 포함해 모든 디렉토리를 뜻하는 와일드카드
+    gulp.src("public/es6/**/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("public/dist"));
+
+});
