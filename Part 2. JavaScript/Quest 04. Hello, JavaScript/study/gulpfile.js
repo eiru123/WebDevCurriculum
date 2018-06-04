bc63413db0c04eb714fc73eb6577187392399014
SeungLee <ls0408@naver.com>
2018-06-04 15:32:57 +0900

    update NOTE.md

--- Part 2. JavaScript/Quest 04. Hello, JavaScript/study/gulpfile.js
+++ Part 2. JavaScript/Quest 04. Hello, JavaScript/study/gulpfile.js
@@ -1,14 +1,14 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
-const eslint = require('gulp-eslint');
+// const eslint = require('gulp-eslint');
 
 gulp.task('default', function(){
 	// node source
 	// babel은 es6 코드를 es5 코드로 변환
 	// gulp 는 디렉토리 구조와 파일 이름을 그대로 유지한다.
-	gulp.src(['es6/**/*.js', 'public/es6/**/*.js'])
-		.pipe(eslint())
-		.pipe(eslint.format());
+	// gulp.src(['es6/**/*.js', 'public/es6/**/*.js'])
+	// 	.pipe(eslint())
+	// 	.pipe(eslint.format());
 
 	gulp.src('es6/**/*.js')
 		.pipe(babel())
