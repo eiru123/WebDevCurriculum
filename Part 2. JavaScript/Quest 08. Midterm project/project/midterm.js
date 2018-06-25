d8dd44ff2ee6881c3cf0eae1f64b24941fa1e09a
SeungLee <ls0408@naver.com>
2018-06-25 11:45:01 +0900

    add midterm files

---
+++ Part 2. JavaScript/Quest 08. Midterm project/project/midterm.js
@@ -0,0 +1,35 @@
+class App{
+    constructor(){
+        this.dom = document.querySelector(".app");
+    }
+}
+
+class addButton{
+    constructor(){
+
+    }
+}
+
+class PlanArea{
+    constructor(){
+        
+    }
+}
+
+class Plan{
+    constructor(){
+        
+    }
+}
+
+class ResultArea{
+    constructor(){
+        
+    }
+}
+
+class Result{
+    constructor(){
+        
+    }
+}
