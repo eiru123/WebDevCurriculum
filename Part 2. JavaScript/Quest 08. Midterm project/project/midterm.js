7a1ac7c5854aabb6be3c2ac3813841d336e4f707
SeungLee <ls0408@naver.com>
2018-06-25 13:51:49 +0900

    update midterm skeleton

--- Part 2. JavaScript/Quest 08. Midterm project/project/midterm.js
+++ Part 2. JavaScript/Quest 08. Midterm project/project/midterm.js
@@ -1,35 +1,80 @@
 class App{
     constructor(){
         this.dom = document.querySelector(".app");
+        this.setApp();
+    }
+    setApp(){
+        const add = new Add();
+        const planArea = new PlanArea();
+        const resultArea = new ResultArea();
+
+        this.dom.appendChild(add.getDom());
+        this.dom.appendChild(planArea.getDom());
+        this.dom.appendChild(resultArea.getDom());
     }
 }
 
-class addButton{
+class Add{
     constructor(){
-
+        this.makeDom();
+    }
+    makeDom(){
+        const template = document.querySelector("#add");
+        this.dom = document.importNode(template.content, true);
+    }
+    getDom(){
+        return this.dom;
     }
 }
 
 class PlanArea{
     constructor(){
-        
+        this.makeDom();
+    }
+    makeDom(){
+        const template = document.querySelector("#plan-area");
+        this.dom = document.importNode(template.content, true);
+    }
+    getDom(){
+        return this.dom;
     }
 }
 
 class Plan{
     constructor(){
-        
+        this.makeDom();
+    }
+    makeDom(){
+        const template = document.querySelector("#plan");
+        this.dom = document.importNode(template.content, true);
+    }
+    getDom(){
+        return this.dom;
     }
 }
 
 class ResultArea{
     constructor(){
-        
+        this.makeDom();
+    }
+    makeDom(){
+        const template = document.querySelector("#result-area");
+        this.dom = document.importNode(template.content, true);
+    }
+    getDom(){
+        return this.dom;
     }
 }
 
 class Result{
     constructor(){
-        
+        this.makeDom();
+    }
+    makeDom(){
+        const template = document.querySelector("#result");
+        this.dom = document.importNode(template.content, true);
+    }
+    getDom(){
+        return this.dom;
     }
 }
