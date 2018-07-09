b344edfb208e39b8dfa46bbd3a1d6a3a904d4017
SeungLee <ls0408@naver.com>
2018-07-09 13:30:15 +0900

    update server.js

--- Part 3. node.js/Quest 11. My little web server/skeleton/server.js
+++ Part 3. node.js/Quest 11. My little web server/skeleton/server.js
@@ -1,5 +1,23 @@
 const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
 
 http.createServer((req, res) => {
 	// TODO: 이 곳을 채워넣으세요..!
+	let reqUrl = url.parse(req.url);
+	if(reqUrl.pathname === "/foo"){
+		let query = querystring.parse(reqUrl.query);
+		console.log('hello ' + query.bar);
+		let tempStr = '';
+		req.on('data', chunk=>{
+			tempStr += chunk;
+		});
+		req.on('end', ()=>{
+			console.log('hello ' + querystring.parse(tempStr).bar);
+		});
+	}
+
+	res.writeHead(200);
+	res.write("hello world!");
+	res.end();
 }).listen(8080);
