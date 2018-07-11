8dcb13b64fb854c4028edea40ae62fdd06fd8e80
SeungLee <ls0408@naver.com>
2018-07-11 10:52:06 +0900

    update server.js

--- Part 3. node.js/Quest 11. My little web server/skeleton/server.js
+++ Part 3. node.js/Quest 11. My little web server/skeleton/server.js
@@ -17,7 +17,7 @@ http.createServer((req, res) => {
 		});
 	}
 
-	res.writeHead(200);
+	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.write("hello world!");
 	res.end();
 }).listen(8080);
