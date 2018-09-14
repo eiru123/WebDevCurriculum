493546fd290d0504f219df79eaf8a4a3f6af50e2
SeungLee <ls0408@naver.com>
2018-09-14 13:55:49 +0900

    update notepad webpack

--- Part 5. Web 2018/Quest 17. Webpack/submit/notepad/client/webpack.config.js
+++ Part 5. Web 2018/Quest 17. Webpack/submit/notepad/client/webpack.config.js
@@ -2,8 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
-
-module.exports = {
+const config = {
   entry: ['@babel/polyfill', './src/main.js'],
   output: {
     path: path.resolve(__dirname, './dist'),
@@ -59,27 +58,34 @@ module.exports = {
     new VueLoaderPlugin()
   ]
 }
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  ])
+module.exports = (env, argv) => {
+  
+  if(argv.mode ==='production'){
+    config.devtool = 'source-map'
+  }
+  return config;
 }
+
+// if (process.env.NODE_ENV === 'production') {
+//   module.exports.devtool = '#source-map'
+//   // http://vue-loader.vuejs.org/en/workflow/production.html
+//   module.exports.plugins = (module.exports.plugins || []).concat([
+//     new webpack.DefinePlugin({
+//       'process.env': {
+//         NODE_ENV: '"production"'
+//       }
+//     }),
+//     // new webpack.optimize.UglifyJsPlugin({
+//     //   sourceMap: true,
+//     //   compress: {
+//     //     warnings: false
+//     //   }
+//     // }),
+//     new webpack.LoaderOptionsPlugin({
+//       minimize: true
+//     })
+//   ])
+// }
 
 
 // webpack4
